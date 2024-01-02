016b3947fcabd787cad9cb14d4f819b86ef90507
Van Chi Hieu <vanchihieu3@gmail.com>
2024-01-02 14:39:30 +0700

    Create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+
 import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -27,6 +30,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
