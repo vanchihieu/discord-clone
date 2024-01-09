e813eb7ca053afce3acd5881adadef03d0ba9656
Van Chi Hieu <vanchihieu3@gmail.com>
2024-01-09 16:21:07 +0700

    Chat Header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
-// import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatHeader } from "@/components/chat/chat-header";
 // import { ChatInput } from "@/components/chat/chat-input";
 // import { ChatMessages } from "@/components/chat/chat-messages";
 // import { MediaRoom } from "@/components/media-room";
@@ -42,11 +42,11 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-      {/* <ChatHeader
+      <ChatHeader
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
-      /> */}
+      />
       {channel.type === ChannelType.TEXT && (
         <>
           {/* <ChatMessages
