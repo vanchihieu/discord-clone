419977a0838a367e298d3bd6822da341cf9968a3
Van Chi Hieu <vanchihieu3@gmail.com>
2024-01-09 15:15:53 +0700

    Channel ID Page

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -21,6 +21,7 @@ const roleIconMap = {
 };
 
 export const ServerMember = ({ member, server }: ServerMemberProps) => {
+
   const params = useParams();
   const router = useRouter();
 
@@ -45,8 +46,8 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
       <p
         className={cn(
           "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-          // params?.memberId === member.id
-        //     "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+          params?.memberId === member.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
         )}
       >
         {member.profile.name}
