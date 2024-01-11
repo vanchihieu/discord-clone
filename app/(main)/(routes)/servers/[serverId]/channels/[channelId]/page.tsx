8f9930835936ccb53a8691b0d989e183803435da
Van Chi Hieu <vanchihieu3@gmail.com>
2024-01-11 11:38:18 +0700

    Chat input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,7 +4,7 @@ import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
-// import { ChatInput } from "@/components/chat/chat-input";
+import { ChatInput } from "@/components/chat/chat-input";
 // import { ChatMessages } from "@/components/chat/chat-messages";
 // import { MediaRoom } from "@/components/media-room";
 import { db } from "@/lib/db";
@@ -63,7 +63,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             paramKey="channelId"
             paramValue={channel.id}
           /> */}
-          {/* <ChatInput
+          <ChatInput
             name={channel.name}
             type="channel"
             apiUrl="/api/socket/messages"
@@ -71,7 +71,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
               channelId: channel.id,
               serverId: channel.serverId,
             }}
-          /> */}
+          />
         </>
       )}
       {/* {channel.type === ChannelType.AUDIO && (
